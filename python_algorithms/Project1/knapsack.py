2bef01398a5148d3186300e151fba3dc4e7dc10c
Jonathan <jonathan.hilgart@gmail.com>
2021-01-27 19:49:33 -0800

    this solution works, not gradescope ready

--- python_algorithms/Project1/knapsack.py
+++ python_algorithms/Project1/knapsack.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # argparse allows the parsing of command line arguments
 import argparse
-from tester import makeinput  # TODO: remove
+
+# from tester import makeinput
 
 # utility functions for cs 6515 projects
 import GA_ProjectUtils as util
@@ -75,6 +76,9 @@ Construct list of tuples of items that should be chosen.
 
 def buildResultList(T, items, maxWt):
 
+    if maxWt <= 0:
+        return []
+
     n_items = len(items)
     final_value_found = T[n_items][maxWt]  # 0 indexed
     list_of_items = []
@@ -119,10 +123,8 @@ def knapsack(items, maxWt):
         for w in weightIter:
             # expand table values by solving subproblem
             table[itmIdx][w] = subProblem(table, w, itmIdx, itemWt, itemVal)
-    print(table[-1][-1], "final value")
-    print(" ")
+
     result_list = buildResultList(table, items, maxWt)
-    print(result_list, " result_list")
     # build list of results - chosen items to maximize value for a given weight
     return result_list
 
@@ -180,82 +182,24 @@ def main():
     util.displayKnapSack(args, itemsChosen)
 
 
+# if __name__ == "__main__":
+#     main()
 def verifyOutput(res, sol, j=-1):
     mysol = set([int(k[0]) for k in res])
-    print(res, " res")
-    print(sol, "sol")
     correct = []
-    weight_seven = [
-        70,
-        73,
-        77,
-        80,
-        82,
-        87,
-        90,
-        94,
-        98,
-        106,
-        110,
-        113,
-        115,
-        118,
-        120,
-    ]
-    value_seven = [
-        135,
-        139,
-        149,
-        150,
-        156,
-        163,
-        173,
-        184,
-        192,
-        201,
-        210,
-        214,
-        221,
-        229,
-        240,
-    ]
-    cacpacity_seven = 750
-
-    total_weight_seven = 0
-    total_value_seven = 0
-    my_weight_seven = 0
-    my_value_seven = 0
-    for idx, s in enumerate(sol):
+    for i, s in enumerate(sol):
         if s > 0:
-            correct.append(idx + 1)
-        if i == 7 and s > 0:
-            total_weight_seven += weight_seven[idx]
-            total_value_seven += value_seven[idx]
-
-    for idx in range(len(res)):
-        my_weight_seven += list(res[idx])[1]
-        my_value_seven += list(res[idx])[2]
-
+            correct.append(i + 1)
     correct = set(correct)
     if mysol == correct:
         print(f"Test case {j} PASSED: {mysol} == {correct}")
-        print(f"mysol = {mysol} correct = {correct}")
-    else:  # The problem with number seven is the backtracing!
-        print(f" total_weight_seven = {total_weight_seven}")
-        print(f"total_value_seven = {total_value_seven}")
-        print(f" my_weight_seven = {my_weight_seven}")
-        print(f" my_value_seven = { my_value_seven} ")
-        print(f"mysol = {mysol} correct = {correct}")
+    else:
         print(f"Test case {j} FAILED: {mysol} != {correct}")
-        return
 
-
-# if __name__ == "__main__":
-#     for i in range(1, 9):
-#         items, W, sol = makeinput(i)
-#         res = knapsack(items, W)
-#         verifyOutput(res, sol, i)
-#     exit(0)
 
 if __name__ == "__main__":
-    main()
+    for i in range(1, 9):
+        items, W, sol = makeinput(i)
+        res = knapsack(items, W)
+        verifyOutput(res, sol, i)
+    exit(0)
